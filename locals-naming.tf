c970bbbf79639fff6b445da0405fca45116a843e
Jérémy MARMOL <jeremy.marmol@fr.clara.net>
2023-02-14 14:44:29 +0100

    AZ-935: Fix variable name

--- locals-naming.tf
+++ locals-naming.tf
@@ -5,6 +5,6 @@ locals {
   rsv_name            = coalesce(var.recovery_vault_custom_name, lower(data.azurecaf_name.rsv.result))
   primary_srf_name    = coalesce(var.primary_site_recovery_fabric_custom_name, lower(data.azurecaf_name.primary_srf.result))
   secondary_srf_name  = coalesce(var.secondary_site_recovery_fabric_custom_name, lower(data.azurecaf_name.secondary_srf.result))
-  primary_srpc_name   = coalesce(var.primary_site_recovery_protection_container, lower(data.azurecaf_name.primary_srpc.result))
-  secondary_srpc_name = coalesce(var.secondary_site_recovery_protection_container, lower(data.azurecaf_name.secondary_srpc.result))
+  primary_srpc_name   = coalesce(var.primary_site_recovery_protection_container_custom_name, lower(data.azurecaf_name.primary_srpc.result))
+  secondary_srpc_name = coalesce(var.secondary_site_recovery_protection_container_custom_name, lower(data.azurecaf_name.secondary_srpc.result))
 }
