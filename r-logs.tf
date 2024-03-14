b8fe11bb6d6af98897519ed96cbe30c4208edb4c
OneGit service account - Renovate <fr-ops4ops+bot-renovate@fr.clara.net>
2024-03-14 16:49:00 +0000

    chore(deps): update terraform claranet/diagnostic-settings/azurerm to ~> 6.5.0

--- r-logs.tf
+++ r-logs.tf
@@ -1,6 +1,6 @@
 module "diagnostics_recovery_vault" {
   source  = "claranet/diagnostic-settings/azurerm"
-  version = "~> 6.4.1"
+  version = "~> 6.5.0"
 
   logs_destinations_ids = coalesce(var.recovery_vault_logs_destinations_ids, var.logs_destinations_ids)
   resource_id           = azurerm_recovery_services_vault.asr_vault.id
