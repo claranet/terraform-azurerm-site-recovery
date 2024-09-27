539f2d3e1e36b16b7b3f6d2f56fa642d738e5f90
Jérôme Respaut <jerome.respaut@claranet.com>
2024-09-27 12:17:19 +0200

    feat: use Claranet "azurecaf" provider

--- versions.tf
+++ versions.tf
@@ -6,7 +6,7 @@ terraform {
       version = "~> 3.114"
     }
     azurecaf = {
-      source  = "aztfmod/azurecaf"
+      source  = "claranet/azurecaf"
       version = "~> 1.1"
     }
     azapi = {
