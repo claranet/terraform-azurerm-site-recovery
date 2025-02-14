af0794129ac40b46267d6f6be01540ea9a464df8
Marc-Antoine Bourlier <ma.bourlier@fr.clara.net>
2025-02-14 16:26:01 +0100

    fix(AZ-1088): fix example

--- examples/main/base.tf
+++ examples/main/base.tf
@@ -2,8 +2,8 @@ module "rg" {
   source  = "claranet/rg/azurerm"
   version = "x.x.x"
 
-  location       = module.azure_region.location
-  location_short = module.azure_region.location_short
+  location       = module.secondary_location.location
+  location_short = module.secondary_location.location_short
   client_name    = var.client_name
   environment    = var.environment
   stack          = var.stack
